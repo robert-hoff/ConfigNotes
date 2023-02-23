3218290dbcc71ed294c53ce96a8848a9bea905bc
Hoff Inc <hoff.rj@gmail.com>
2023-02-23 21:35:38 +0000

    create settings file, storing some form properties between runs

--- CSharpSnippets/CSharpSnippets/FormElements/MainForm.cs
+++ CSharpSnippets/CSharpSnippets/FormElements/MainForm.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel;
+using System.Configuration;
+
 namespace CSharpSnippets.FormElements
 {
     public partial class MainForm : Form
     {
+        private readonly FormSettings Settings;
+
         public MainForm()
         {
             InitializeComponent();
+            // restore previous size and position of the application window
+            Settings = new FormSettings();
+            StartPosition = FormStartPosition.Manual;
+            Location = Settings.FormLocation;
+            Size = Settings.FormSize;
         }
 
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
@@ -14,6 +24,32 @@ namespace CSharpSnippets.FormElements
                 Close();
             }
             return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Settings.FormLocation = Location;
+            Settings.FormSize = Size;
+            Settings.Save();
+            base.OnClosing(e);
+        }
+
+        private sealed class FormSettings : ApplicationSettingsBase
+        {
+            [UserScopedSetting()]
+            [DefaultSettingValue("300, 100")]
+            public Point FormLocation
+            {
+                get { return (Point) this[nameof(FormLocation)]; }
+                set { this[nameof(FormLocation)] = value; }
+            }
+            [UserScopedSetting()]
+            [DefaultSettingValue("800, 500")]
+            public Size FormSize
+            {
+                get { return (Size) this[nameof(FormSize)]; }
+                set { this[nameof(FormSize)] = value; }
+            }
         }
     }
 }
