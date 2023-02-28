3a1e7245cbc0eccbed6b4d05def7a33070b63bc0
Hoff Inc <hoff.rj@gmail.com>
2023-02-28 01:17:20 +0000

    snippets and class template

---
+++ CSharpConfig/ClassTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+namespace $rootnamespace$
+{
+    public class $safeitemrootname$
+    {
+        public static void Run() {
+            Trial1();
+        }
+
+        public static void Trial1() {
+
+        }
+    }
+}
+
